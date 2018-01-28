7d6218b010258e8907ff8f410798210d796ce562
Alexis Milon <amilon@hawk.iit.edu>
2018-01-28 17:52:08 -0600

    Add function to determine if the user is 18 years or older

---
+++ pp-01/site.js
@@ -0,0 +1,20 @@
+$.noConflict();
+(function($) {
+  $('#contact-form').on('submit', function(e) {
+    e.preventDefault(); //prevent link in url
+    var user = new Date(document.getElementById('dob').value); //get person's dob
+    var current = new Date(); //get current date
+    var dob = user.getFullYear(); //get the year of the person's dob
+    var today = current.getFullYear(); //get the current year
+    var age = today - dob; //subtract the peron's dob from current year
+
+    if(age < 18) {
+  $('#contact-form').append('You need to be 18 years old or older.'
+);
+}
+    if (age >= 18) {
+  $('#contact-form').append('Your form has been submitted!'
+);
+}
+});
+})(jQuery);
