f8091dded7618a15a2976938934d77e60e6c08d3
Alexis Milon <amilon@hawk.iit.edu>
2018-01-29 23:10:47 -0600

    Fix errors based on linter

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,20 +1,20 @@
 $.noConflict();
 (function($) {
+  var user = new Date(document.getElementById('dob').value), // get person's dob
+  var current = new Date(), //  get current date
+  var dob = user.getFullYear(), //  get the year of the person's dob
+  var today = current.getFullYear(), // get the current year
+  var age = today - dob; // subtract the peron's dob from current year
   $('#contact-form').on('submit', function(e) {
-    e.preventDefault(); //prevent link in url
-    var user = new Date(document.getElementById('dob').value); //get person's dob
-    var current = new Date(); //get current date
-    var dob = user.getFullYear(); //get the year of the person's dob
-    var today = current.getFullYear(); //get the current year
-    var age = today - dob; //subtract the peron's dob from current year
+    e.preventDefault(); //  prevent link in url
 
     if(age < 18) {
-  $('#contact-form').append('You need to be 18 years old or older.'
-);
-}
+      $('#contact-form').append('You need to be 18 years old or older.'
+      );
+    }
     if (age >= 18) {
-  $('#contact-form').append('Your form has been submitted!'
-);
-}
-});
+      $('#contact-form').append('Your form has been submitted!'
+      );
+    }
+  });
 })(jQuery);
